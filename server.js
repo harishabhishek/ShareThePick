c9886c278263d0e24e99cc451d14ef0bc7103411
harishabhishek <harish2@illinois.edu>
2015-05-10 13:44:49 -0500

    Fixed the small event bug

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ var port = process.env.PORT || 4000;
 
 //Allow CORS so that backend and frontend could pe put on different servers
 var allowCrossDomain = function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", ["http://do.di4fun.com:3000", "http://localhost:3000"]);
+  res.header("Access-Control-Allow-Origin", "http://do.di4fun.com:3000");
   res.header("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
   res.header("Access-Control-Allow-Credentials","true");
